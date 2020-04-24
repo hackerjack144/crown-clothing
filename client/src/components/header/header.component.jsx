ef0f0065bff99fcfb31ba580affb8c150201ead0
hackjack123 <jaiswal.dheeraj123@gmail.com>
2020-04-24 21:00:27 +0530

    moved code to client folder and wrote server code that separate stripe backend code and frontend calling.

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import { withRouter } from 'react-router-dom';
+
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
-import { signOutStart } from '../../redux/user/user.actions';
+import {signOutStart}  from '../../redux/user/user.actions';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import {HeaderContainer, LogoContainer, OptionContainer, OptionLink} from  './header.styles';
 
 const Header = ({ currentUser, hidden, signOutStart }) => (
-
+ 
   <HeaderContainer>
 
     <LogoContainer to='/'>
@@ -57,4 +59,4 @@ const mapStateToProps = createStructuredSelector({
 const mapDispatchToProps = dispatch =>({
   signOutStart: () => dispatch(signOutStart())
 })
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header));
